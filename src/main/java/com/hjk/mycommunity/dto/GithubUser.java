dbfd6ad722770968e2b2e22b27ace7f68aecad57
hjk <2088624235@qq.com>
2023-02-19 10:30:59 +0800

    使用okhttp，登录github获取用户信息

---
+++ src/main/java/com/hjk/mycommunity/dto/GithubUser.java
@@ -0,0 +1,16 @@
+package com.hjk.mycommunity.dto;
+
+import lombok.Data;
+
+/**
+ * git
+ *
+ * @author 20886 Email:
+ * @since 2023/02/16 21:47
+ */
+@Data
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+}
